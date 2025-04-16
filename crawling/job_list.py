0497ff57bde259ae1c3a6bc807abea1bdc6c5221
BitedRadish <a980831@naver.com>
2025-04-16 10:55:42 +0900

    feat : job_list 추가

---
+++ crawling/job_list.py
@@ -0,0 +1,61 @@
+FRONT_STACK_LIST = [
+    "react", "next.js", "vue", "nuxt.js", "angular", "svelte", "solid.js", "astro", "qwik",
+    "typescript", "javascript", "tailwindcss", "bootstrap", "material-ui", "chakra-ui",
+    "styled-components", "emotion", "sass", "less",
+    "redux", "zustand", "mobx", "recoil", "jotai",
+    "webpack", "vite", "rollup", "parcel",
+    "babel", "eslint", "prettier",
+    "jest", "cypress", "playwright", "storybook",
+    "graphql", "apollo client", "urql",
+    "pwa", "webassembly", "three.js", "d3.js", "chart.js"
+]
+BACK_STACK_LIST = [
+    "node.js", "express", "nest.js", "koa",
+    "spring", "spring boot", "java", "kotlin",
+    "django", "flask", "fastapi", "python",
+    "ruby on rails", "laravel", "php",
+    "asp.net", "c#", "go", "gin", "echo",
+    "graphql", "apollo server", "rest api",
+    "mysql", "postgresql", "mariadb", "mongodb", "redis", "cassandra", "dynamodb",
+    "elasticsearch", "kafka", "rabbitmq", "nats", "grpc", "protobuf",
+    "docker", "kubernetes", "nginx", "apache",
+    "prisma", "typeorm", "sequelize", "sqlalchemy", "mongoose"
+]
+IOS_STACK_LIST = [
+    "swift", "objective-c", "swiftui", "uikit",
+    "coredata", "realm", "combine", "rxswift",
+    "alamofire", "moya", "firebase", "coreml", "arkit", "realitykit",
+    "xcode", "fastlane", "cocoapods", "carthage", "sourcery",
+    "xctest", "quick", "nimble", "metrickit"
+]
+ANDROID_STACK_LIST = [
+    "kotlin", "java", "android sdk", "android jetpack",
+    "jetpack compose", "xml", "room", "realm",
+    "retrofit", "okhttp", "dagger", "hilt", "koin",
+    "rxjava", "coroutines", "glide", "picasso", "coil",
+    "firebase", "android studio", "gradle", "maven",
+    "espresso", "junit", "mockk", "leakcanary"
+]
+GAME_STACK_LIST = [
+    "unity", "unreal engine", "godot", "cryengine", "cocos2d-x", "gamemaker studio", "phaser", "monogame", "libgdx",
+    "blender", "maya", "3ds max",
+    "openGL", "directX", "vulkan",
+    "fmod", "wwise", "hlsl", "glsl",
+    "steamworks", "xbox live sdk", "psn sdk"
+]
+SECURITY_STACK_LIST = [
+    "kali linux", "wireshark", "nmap", "metasploit", "burp suite", "nessus", "openvas", "snort", "suricata",
+    "osquery", "splunk", "elk stack", "nagios", "zabbix",
+    "hashcat", "john the ripper", "sqlmap", "aircrack-ng",
+    "cloudflare", "okta", "auth0", "mfa", "sso", "saml", "oauth2",
+    "veracrypt", "keepass", "gpg", "tor", "vpn"]
+
+CLOUD_STACK_LIST = [
+    "aws", "gcp", "azure", "digitalocean", "oracle cloud",
+    "docker", "kubernetes", "helm", "istio", "nginx",
+    "terraform", "cloudformation", "pulumi", "ansible", "chef", "puppet",
+    "jenkins", "github actions", "gitlab ci", "circleci", "argo cd", "tekton",
+    "prometheus", "grafana", "datadog", "new relic", "sentry", "loki",
+    "aws lambda", "google cloud functions", "azure functions",
+    "s3", "ec2", "rds", "cloud run", "cloudflare", "firebase"
+]
