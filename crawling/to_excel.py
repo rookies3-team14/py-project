180f79ba8090b79b9fdac1a82794e1dabff1580c
정재운 <jaeun101501@gmail.com>
2025-04-17 16:31:13 +0900

    fix:텍스트 저장 수정

--- crawling/to_excel.py
+++ crawling/to_excel.py
@@ -15,24 +15,31 @@ cloud_list = job_list.CLOUD_STACK_LIST
 entire_list = [front_list, back_list, ios_list, cross_list, android_list, game_list, security_list, cloud_list]
 
 
-def count_data(data_list, elist, count_total):
+def count_data(data_list, elist, count_total, stack_names):
     count_s = {}
-    for d in data_list:
+    data = data_list['text']
+    for d in data:
         for e in elist:  
             if d.strip() == e.strip():
                 if d not in count_s:
                     count_s[d] = 1
+                    stack_names.append(d)
+    
     count_total.append(count_s)
+  
 
 
  # 파일 존재 여부를 체크하기 위한 모듈
 
 def write_excel(data_list):
     count_total = list()
-    for e in entire_list:
-        count_data(data_list, e, count_total)
+    stack_names = list()
 
+    for e in entire_list:
+        count_data(data_list, e, count_total, stack_names)
     print(count_total)
+    print(stack_names)
+    write_excel2(data_list, stack_names)
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     excel_dir = os.path.join(BASE_DIR, '..', 'data', 'excel')
@@ -70,7 +77,7 @@ def write_excel(data_list):
                 df.to_excel(writer, sheet_name=sheet)
 
 
-def write_excel2(data):
+def write_excel2(data, stack_names):
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     excel_dir = os.path.join(BASE_DIR, '..', 'data', 'excel')
     
@@ -78,7 +85,7 @@ def write_excel2(data):
     save_path = os.path.join(excel_dir, 'RecruitmentNotice.xlsx')
 
 
-    data['text'] = ','.join(data['text']) 
+    data['text'] = ','.join(stack_names) 
     df = pd.DataFrame([data])
     
     if Path(save_path).exists():
@@ -88,24 +95,11 @@ def write_excel2(data):
     else:
         df.to_excel(save_path, index = False)
 
-data = [{
-        'companyName': '(주)루티너리',
-        'title': '[Dev] RN Developer',
-        'recruitUrl': 'https://www.jobplanet.co.kr/job/search?posting_ids%5B%5D=1283031',
-        'annual': '경력',
-        'text': ['85', 'dev', 'spotlight', 'react', 'native', 'react', 'native', 'typescript', 
-                'mobx', 'zustand', 'firebase', 'react', 'query', 'react', 'a', 'to', '70']
-},
-{
-        'companyName': '(주)테스트트',
-        'title': '[Dev] 테스트 Developer',
-        'recruitUrl': 'https://www.jobplanet.co.kr/job/search?posting_ids%5B%5D=1283031',
-        'annual': '신입입',
-        'text': ['react', 'a', 'to', '70']
-},
+data = [{'companyName': '(주)디셈버앤컴퍼니', 'title': '핀트(fint) 프론트엔드(front-end) 개발자', 'recruitUrl': 'https://www.jobplanet.co.kr/job/search?posting_ids%5B%5D=1290918', 'annual': '경력', 'text': ['december', 'company', 'fint', 'it', 'togetherness', 'invest', 'together', 'build', 'together', 'grow', 'together', 'change', 'together', 'playground', 'caring', 'attitude', 'meta', 'cognition', '1', '2', '3', '4', '5', '6', 'irp', 'seo', 'crm', 'preface', 'react', 'typescript', 'next', 'js', 'react', 'query', 'jotai', 'emotion', 'yarn', 'berry', 'pnpm', 'turborepo', 'webpack', 'esbuild', 'swc', 'babel', 'ci', 'cd', 'gitlab', 'ci', 'cd', 'react', 'vue', 'angular', 'spa', 'html', 'css', 'git', 'asana', 'confluence', 'slack', 'figma', 'react', 'typescript', 'next', 'js', 'react', 'query', 'jotai', 'emotion', 'yarn', 'berry', 'pnpm', 'turborepo', 'webpack', 'esbuild', 'swc', 'babel', 'ci', 'cd', 'gitlab', 'ci', 'cd', 'typescript', 'ssr', 'ui', 'webpack', '1', '2', '3', '4', '5', '231', 'west']}
+,{'companyName': '(주)버즈빌', 'title': '[광고 추천팀] 백엔드 개발자 (Engineering Manager, Team Lead)', 'recruitUrl': 'https://www.jobplanet.co.kr/job/search?posting_ids%5B%5D=1288726', 'annual': '경력', 'text': ['saas', 'sk', 'cj', 'ai', 'saas', '4', 'dsp', 'demand', 'side', 'platform', 'a', 'https', 'www', 'buzzvil', 'com', 'career', 'jwcvgiva7dxl7mrbcsknf', 'https', 'www', 'youtube', 'com', 'watch', 'v', '4txlnymetis', 'https', 'www', 'buzzvil', 'com', 'x', 'ceo', 'https', 'www', 'youtube', 'com', 'watch', 'v', '3rtwwkg8cw8', 'https', 'www', 'youtube', 'com', 'watch', 'v', 'cmduwbm3kkc', 'a', 'b', 'ml', 'go', 'python', 'mysql', 'dynamodb', 'redis', 'elasticsearch', 'kafka', 'kubernetes', 'grpc', 'rest', 'api', 'github', 'action', 'spinnaker', 'jira', 'confluence', 'slack', 'github', 'datadog', 'grafana', 'prometheus', 'loki', 'aws', 'cloudwatch', 'aws', 'gcp', 'python', 'golang', 'apache', 'kafka', 'ceo', '1', '1', '2', '3', '4', 'gloria', 'lee', 'buzzvil', 'com', 'x', '272', '2', '3', '4']}
 
 ]
 for d in data:
-    write_excel2(d)
+    write_excel(d)
     print("생성함")
         
