ddfcb628650feed26d1914a9ff1ac43c0540da6f
BitedRadish <a980831@naver.com>
2025-04-17 16:35:06 +0900

    feat : 임시 커밋

--- crawling/main.py
+++ crawling/main.py
@@ -1,22 +1,30 @@
 from job_planet_scraper import crawl_jobs
 from get_recruitment_text import get_jobplanet_recruitment_text
+from to_excel import write_excel2
 import json
 
-if __name__ == "__main__":
-    all_jobs = []
+# if __name__ == "__main__":
+#     all_jobs = []
 
-    for i in range(1, 3):  # 테스트용으로 2페이지만
-        url = f"https://www.jobplanet.co.kr/api/v3/job/postings?occupation_level1=&occupation_level2=11905,11907,11904,11906,11610,11911,11609&years_of_experience=&review_score=&job_type=&city=&education_level_id=&order_by=aggressive&page={i}&page_size=8"
-        jobs = crawl_jobs(url)
-        all_jobs.extend(jobs)
+#     for i in range(1, 2):  # 테스트용으로 2페이지만
+#         url = f"https://www.jobplanet.co.kr/api/v3/job/postings?occupation_level1=&occupation_level2=11905,11907,11904,11906,11610,11911,11609&years_of_experience=&review_score=&job_type=&city=&education_level_id=&order_by=aggressive&page={i}&page_size=8"
+#         jobs = crawl_jobs(url)
+#         all_jobs.extend(jobs)
 
-    for job in all_jobs:
-        data=get_jobplanet_recruitment_text(job)
-        
-    
-    
-    # if all_jobs:
-    #     print("✅ 수집된 공고 수:", len(all_jobs))
-    #     print("🎯 예시 공고:", json.dumps(all_jobs[:5], ensure_ascii=False, indent=2))
-    # else:
-    #     print("❌ 공고를 수집하지 못했습니다.")
+#     print(all_jobs)
+#     for job in all_jobs:
+#         data = get_jobplanet_recruitment_text(job)
+#         print(data)
+#         if len(data["text"]) > 0:
+#             write_excel2(data)
+
+# if all_jobs:
+#     print("✅ 수집된 공고 수:", len(all_jobs))
+#     print("🎯 예시 공고:", json.dumps(all_jobs[:5], ensure_ascii=False, indent=2))
+# else:
+#     print("❌ 공고를 수집하지 못했습니다.")
+
+write_excel2({'companyName': '(주)디셈버앤컴퍼니', 'title': '핀트(fint) 프론트엔드(front-end) 개발자', 'recruitUrl': 'https://www.jobplanet.co.kr/job/search?posting_ids%5B%5D=1290918', 'annual': '경력', 'text': ['december', 'company', 'fint', 'it', 'togetherness', 'invest', 'together', 'build', 'together', 'grow', 'together', 'change', 'together', 'playground', 'caring', 'attitude', 'meta', 'cognition', '1', '2', '3', '4', '5', '6', 'irp', 'seo', 'crm', 'preface', 'react', 'typescript', 'next', 'js', 'react', 'query',
+             'jotai', 'emotion', 'yarn', 'berry', 'pnpm', 'turborepo', 'webpack', 'esbuild', 'swc', 'babel', 'ci', 'cd', 'gitlab', 'ci', 'cd', 'react', 'vue', 'angular', 'spa', 'html', 'css', 'git', 'asana', 'confluence', 'slack', 'figma', 'react', 'typescript', 'next', 'js', 'react', 'query', 'jotai', 'emotion', 'yarn', 'berry', 'pnpm', 'turborepo', 'webpack', 'esbuild', 'swc', 'babel', 'ci', 'cd', 'gitlab', 'ci', 'cd', 'typescript', 'ssr', 'ui', 'webpack', '1', '2', '3', '4', '5', '231', 'west']})
+write_excel2({'companyName': '(주)디셈버앤컴퍼니', 'title': '핀트(fint) 프론트엔드(front-end) 개발자', 'recruitUrl': 'https://www.jobplanet.co.kr/job/search?posting_ids%5B%5D=1290918', 'annual': '경력', 'text': ['december', 'company', 'fint', 'it', 'togetherness', 'invest', 'together', 'build', 'together', 'grow', 'together', 'change', 'together', 'playground', 'caring', 'attitude', 'meta', 'cognition', '1', '2', '3', '4', '5', '6', 'irp', 'seo', 'crm', 'preface', 'react', 'typescript', 'next', 'js', 'react', 'query',
+             'jotai', 'emotion', 'yarn', 'berry', 'pnpm', 'turborepo', 'webpack', 'esbuild', 'swc', 'babel', 'ci', 'cd', 'gitlab', 'ci', 'cd', 'react', 'vue', 'angular', 'spa', 'html', 'css', 'git', 'asana', 'confluence', 'slack', 'figma', 'react', 'typescript', 'next', 'js', 'react', 'query', 'jotai', 'emotion', 'yarn', 'berry', 'pnpm', 'turborepo', 'webpack', 'esbuild', 'swc', 'babel', 'ci', 'cd', 'gitlab', 'ci', 'cd', 'typescript', 'ssr', 'ui', 'webpack', '1', '2', '3', '4', '5', '231', 'west']})
--- crawling/to_excel.py
+++ crawling/to_excel.py
@@ -1,31 +1,32 @@
 import os
 import job_list
 import pandas as pd
-from pathlib import Path 
+from pathlib import Path
 
 front_list = job_list.FRONT_STACK_LIST
-back_list =job_list.BACK_STACK_LIST
+back_list = job_list.BACK_STACK_LIST
 ios_list = job_list.IOS_STACK_LIST
 cross_list = job_list.CROSS_STACK_LIST
 android_list = job_list.ANDROID_STACK_LIST
-game_list =job_list.GAME_STACK_LIST
+game_list = job_list.GAME_STACK_LIST
 security_list = job_list.SECURITY_STACK_LIST
 cloud_list = job_list.CLOUD_STACK_LIST
 
-entire_list = [front_list, back_list, ios_list, cross_list, android_list, game_list, security_list, cloud_list]
+entire_list = [front_list, back_list, ios_list, cross_list,
+               android_list, game_list, security_list, cloud_list]
 
 
 def count_data(data_list, elist, count_total):
     count_s = {}
     for d in data_list:
-        for e in elist:  
+        for e in elist:
             if d.strip() == e.strip():
                 if d not in count_s:
                     count_s[d] = 1
     count_total.append(count_s)
 
-
  # 파일 존재 여부를 체크하기 위한 모듈
+
 
 def write_excel(data_list):
     count_total = list()
@@ -33,13 +34,14 @@ def write_excel(data_list):
         count_data(data_list, e, count_total)
 
     print(count_total)
-    
+
     file_name = '../data/excel/StackList.xlsx'
-    sheet_names = ['FRONT_STACK_LIST','BACK_STACK_LIST','IOS_STACK_LIST', 'CROSS_STACK_LIST', 'ANDROID_STACK_LIST', 'GAME_STACK_LIST', 
-                'SECURITY_STACK_LIST', 'CLOUD_STACK_LIST']
+    sheet_names = ['FRONT_STACK_LIST', 'BACK_STACK_LIST', 'IOS_STACK_LIST', 'CROSS_STACK_LIST', 'ANDROID_STACK_LIST', 'GAME_STACK_LIST',
+                   'SECURITY_STACK_LIST', 'CLOUD_STACK_LIST']
     df_list = list()
     for i in range(len(sheet_names)):
-        df = pd.DataFrame(list(count_total[i].items()), columns= ['stack', 'count'])
+        df = pd.DataFrame(list(count_total[i].items()), columns=[
+                          'stack', 'count'])
         df.set_index('stack', inplace=True)  # stack을 인덱스로 설정
         df_list.append(df)
 
@@ -47,12 +49,13 @@ def write_excel(data_list):
     if Path(file_name).exists():
         try:
             for i in range(len(sheet_names)):
-                old_df = pd.read_excel(file_name, sheet_name=sheet_names[i], index_col=0)
+                old_df = pd.read_excel(
+                    file_name, sheet_name=sheet_names[i], index_col=0)
                 total_df_list.append(old_df.add(df_list[i], fill_value=0))
         except:
             # 시트가 엑셀 파일 안에 없을 때
             total_df_list = df_list
-    #엑셀 파일 없을 때
+    # 엑셀 파일 없을 때
     else:
         total_df_list = df_list
 
@@ -69,34 +72,42 @@ def write_excel(data_list):
 def write_excel2(data):
     file_name = '../data/excel/RecruitmentNotice.xlsx'
     sheet = 'Recruitment information'
-    if not os.path.exists(file_name):
-        os.makedirs(file_name)
-    data['text'] = ', '.join(data['text']) 
-    df = pd.DataFrame(list(data))
+
+    # 📌 디렉토리 경로만 따로 분리
+    dir_path = os.path.dirname(file_name)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    # 문자열 리스트를 하나의 문자열로 변환
+    data['text'] = ', '.join(data['text'])
+
+    # DataFrame 생성
+    df = pd.DataFrame([data])  # ⚠️ list(data) → [data] (열로 변환되는 문제 해결)
 
     if Path(file_name).exists():
         with pd.ExcelWriter(file_name, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-            df.to_excel(file_name, sheet_name=sheet, index = False, header = False)
+            df.to_excel(writer, sheet_name=sheet, index=False, header=False)
     else:
-        df.to_excel(file_name, sheet_name=sheet, engine='openpyxl', index = False)
+        df.to_excel(file_name, sheet_name=sheet,
+                    engine='openpyxl', index=False)
+
 
 data = [{
         'companyName': '(주)루티너리',
         'title': '[Dev] RN Developer',
         'recruitUrl': 'https://www.jobplanet.co.kr/job/search?posting_ids%5B%5D=1283031',
         'annual': '경력',
-        'text': ['85', 'dev', 'spotlight', 'react', 'native', 'react', 'native', 'typescript', 
-                'mobx', 'zustand', 'firebase', 'react', 'query', 'react', 'a', 'to', '70']
-},
-{
+        'text': ['85', 'dev', 'spotlight', 'react', 'native', 'react', 'native', 'typescript',
+                 'mobx', 'zustand', 'firebase', 'react', 'query', 'react', 'a', 'to', '70']
+        },
+        {
         'companyName': '(주)테스트트',
         'title': '[Dev] 테스트 Developer',
         'recruitUrl': 'https://www.jobplanet.co.kr/job/search?posting_ids%5B%5D=1283031',
         'annual': '신입입',
         'text': ['react', 'a', 'to', '70']
-},
+        },
 
-]
+        ]
 for d in data:
     write_excel2(d)
-        
