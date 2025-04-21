c82751dc70b025b867e22f210d4759dab4d73ccd
정재운 <jaeun101501@gmail.com>
2025-04-21 13:30:18 +0900

    refactor: to_excel, seperate_jobplanet_excel 리팩토링 및 filter_data 추가

--- crawling/seperate_jobplanet_excel.py
+++ crawling/seperate_jobplanet_excel.py
@@ -2,18 +2,17 @@ import pandas as pd
 import os
 import job_list
 
-front_list = job_list.FRONT_STACK_LIST
-back_list = job_list.BACK_STACK_LIST
-ios_list = job_list.IOS_STACK_LIST
-cross_list = job_list.CROSS_STACK_LIST
-android_list = job_list.ANDROID_STACK_LIST
-game_list = job_list.GAME_STACK_LIST
-security_list = job_list.SECURITY_STACK_LIST
-cloud_list = job_list.CLOUD_STACK_LIST
+entire_list = job_list.entire_list
 
-entire_list = [front_list, back_list, ios_list, cross_list,
-               android_list, game_list, security_list, cloud_list]
+def get_path(filename):    
+    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    excel_dir = os.path.join(BASE_DIR, 'data', 'excel')
+    os.makedirs(excel_dir, exist_ok=True)
+    save_path = os.path.join(excel_dir, filename)
 
+    return save_path
+
+#총 stack을 category 별로 다시 나누기
 def seperate_category(stack_df, categories):
     categorized_data = []
     count_dict = dict(zip(stack_df["stack"], stack_df["count"]))
@@ -29,23 +28,17 @@ def seperate_category(stack_df, categories):
     new_df = pd.DataFrame(categorized_data, columns=["category", "stack", "count"])
     return new_df
 
-def save_change():
-    stack_path ='data/excel/StackList.xlsx'
+#세분화 한 데이터 엑셀파일에 덮어씌우기
+def save_change(stack_path):
     sheet_names = pd.ExcelFile(stack_path).sheet_names
     stack_df_list = [pd.read_excel(stack_path, sheet_name=sheet) for sheet in sheet_names]
+    save_path = get_path('StackList_Transformed.xlsx')
 
     new_dataframes = []
     for e, sheet_name, stack_df in zip(entire_list, sheet_names, stack_df_list):
         new_df = seperate_category(stack_df, e)
         new_dataframes.append((sheet_name, new_df))
-
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    excel_dir = os.path.join(BASE_DIR, '..', 'data', 'excel')
-    os.makedirs(excel_dir, exist_ok=True)
-    save_path = os.path.join(excel_dir, 'StackList_Transformed.xlsx')
+    
     with pd.ExcelWriter(save_path, engine="openpyxl") as writer:
         for sheet_name, new_df in new_dataframes:
             new_df.to_excel(writer, index=False, sheet_name=sheet_name)
-            print("생성됨")
-
-# save_change()
